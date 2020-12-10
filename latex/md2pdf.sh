6617a28a1e943df36a1b6e3a45496ba73ac84487
Ralph W. Crosby <ralph.crosby@navy.mil>
2020-12-10 14:55:06 -0500

    UPdateds to md2pdf, vscode cheat sheet

--- latex/md2pdf.sh
+++ latex/md2pdf.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # md2pdf filename title author date
 
@@ -8,14 +8,29 @@ rootname="${filename%%.*}"
 
 thisdir=`dirname "$0"`
 
-title=${2:-$filename}
 author=${3:-"Ralph W. Crosby, PhD."}
 date=${4:-`date`}
-config=${md2pdf_config:-$thisdir/Md2PdfPandocConfig.yaml}
 
-pandoc -V title="$title" \
-       -V date="$date" \
-       -V author="$author" \
-       -o $rootname.pdf \
-       "$config" \
-       $infile
+config=${md2pdf_config:-$thisdir/Md2PdfPandocConfig.yaml}
+template=${md2pdf_template:-$thisdir/md2pdf.pandoc.template}
+
+
+if [ -v 2 ] ; then
+       pandoc -V date="$date" \
+              -V author="$author" \
+              -V title="$2" \
+              -o $rootname.pdf \
+              --template="$template" \
+              --pdf-engine=lualatex \
+              "$config" \
+              $infile 
+else
+       pandoc -V date="$date" \
+              -V author="$author" \
+              -o $rootname.pdf \
+              --template="$template" \
+              --pdf-engine=lualatex \
+              "$config" \
+              $infile 
+fi
+
---
+++ vscode/README.md
@@ -0,0 +1,26 @@
+---
+numbersections: False
+diststmt: 'Dist A: Approved for public release: distribution unlimited'
+title: VSCODE information
+---
+
+# Extensions
+
+- Center Editor Window
+
+# Keyboard
+
+https://code.visualstudio.com/shortcuts/keyboard-shortcuts-macos.pdf
+
+| Key              | Description |
+|-                 |-|
+| \DejaSans ⌘ d    | Extend selection |
+| \DejaSans ^ -    | Previous location |
+| \DejaSans ^ _    | Next location |
+| \DejaSans ^ l    | Center editor window |
+| \DejaSans ^ PgUp | Scroll up line |
+| \DejaSans ^ PgDn | Scroll down line |
+| \DejaSans ⌘ PgUp | Scroll up page |
+| \DejaSans ⌘ PgDn | Scroll down page |
+| \DejaSans ⌥⌘↑    | Insert cursor up |
+| \DejaSans ⌥⌘↓    | Insert cursor down |
