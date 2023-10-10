ea289c66e1efe250d64581c1f56bb1b0adda63e3
Ralph W. Crosby <rcrosby@camgian.com>
2023-10-10 14:17:47 -0400

    Fixed aliases for wsl

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,8 +1,14 @@
 # My aliases
 
-alias b="bat -Ppn"
+if [ $(command -v batcat) ]; then
+    mybat=batcat
+else
+    mybat=bat
+fi
+alias b="$mybat -Ppn"
 
 [ $(command -v lesspipe.sh) ] && export LESSOPEN="|$(which lesspipe.sh) %s"
+[ $(command -v lesspipe) ] && export LESSOPEN="|$(which lesspipe) %s"
 alias l=less 
 
 alias mc="mc -X --nosubshell"
