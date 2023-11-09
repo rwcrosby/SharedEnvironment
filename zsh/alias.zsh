1bda08f08e13aad33d5b93cccddb0997c0f5ecd0
Ralph Crosby <rcrosby@camgian.com>
2023-11-09 09:32:39 -0500

    Added support for the highlight

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -9,6 +9,8 @@ alias b="$mybat -Ppn"
 
 [ $(command -v lesspipe.sh) ] && export LESSOPEN="|$(which lesspipe.sh) %s"
 [ $(command -v lesspipe) ] && export LESSOPEN="|$(which lesspipe) %s"
+[ $(command -v highlight) ] && 	export LESSOPEN="| $(which highlight) %s --out-format xterm256 -l --force -s solarized-light --no-trailing-nl"
+
 alias l=less 
 
 alias mc="mc -X --nosubshell"
