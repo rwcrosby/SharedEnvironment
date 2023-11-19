6c63b5429136011fe64c9f957fdcf9459635eaa8
Ralph Crosby <rwc@cs.tamu.edu>
2023-11-19 18:02:31 -0500

    Enabled dircolors in zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -11,8 +11,13 @@ alias b="$mybat -Ppn"
 [ $(command -v lesspipe) ] && export LESSOPEN="|$(which lesspipe) %s"
 [ $(command -v highlight) ] && 	export LESSOPEN="| $(which highlight) %s --out-format xterm256 -l --force -s solarized-light --no-trailing-nl"
 
-alias l=less 
 
 alias mc="mc -X --nosubshell"
 
 alias kc=kubectl
+
+alias l=less 
+
+alias la='ls -lAh --color=auto'
+alias ll='ls -lh --color=auto'
+alias ls='ls --color=auto'
